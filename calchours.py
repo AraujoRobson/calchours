9d92bf0dadad5b259b217c8d784a826f46efdc59
Robson Araujo <roobson.araujo02@gmail.com>
2022-04-11 14:22:31 -0300

    recebe e formata hora

--- calchours.py
+++ calchours.py
@@ -1,2 +1,10 @@
-def calchours():
-    pass
+import datetime as dt
+
+hora_1 = input(str('(HH:MM:SS) Hora 1: ')) #recebe primeira hora
+hora_2 = input(str('(HH:MM:SS) Hora 2: ')) #recebe segunda hora
+time_1 = dt.datetime.strptime(hora_1,"%H:%M:%S") #formata a primeira hora
+time_2 = dt.datetime.strptime(hora_2,"%H:%M:%S") #formata a segunda hora
+
+time_interval = time_1 - time_2
+
+print(time_interval)
