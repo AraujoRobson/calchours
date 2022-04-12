cb75d697afd283160fbaddd60a8d486d3247c339
Robson Araujo <roobson.araujo02@gmail.com>
2022-04-12 18:32:06 -0300

    edit

--- calchours.py
+++ calchours.py
@@ -1,10 +1,26 @@
-import datetime as dt
+from datetime import datetime as dt
 
-hora_1 = input(str('(HH:MM:SS) Hora 1: ')) #recebe primeira hora
-hora_2 = input(str('(HH:MM:SS) Hora 2: ')) #recebe segunda hora
-time_1 = dt.datetime.strptime(hora_1,"%H:%M:%S") #formata a primeira hora
-time_2 = dt.datetime.strptime(hora_2,"%H:%M:%S") #formata a segunda hora
+carga = input(str('(HH:MM) Carga horária diária: ')) #recebe carga horaria
+hora_1 = input(str('(HH:MM) Entrada 1: ')) #recebe primeira hora
+hora_2 = input(str('(HH:MM) Saída   1: ')) #recebe segunda hora
+hora_3 = input(str('(HH:MM) Entrada 2: ')) #recebe segunda hora
+hora_4 = input(str('(HH:MM) Saída   2: ')) #recebe segunda hora
 
-time_interval = time_1 - time_2
+carga1 = dt.strptime(carga,"%H:%M") #formata a carga horária
+time_1 = dt.strptime(hora_1,"%H:%M") #formata a primeira hora
+time_2 = dt.strptime(hora_2,"%H:%M") #formata a segunda hora
+time_3 = dt.strptime(hora_3,"%H:%M") #formata a primeira hora
+time_4 = dt.strptime(hora_4,"%H:%M") #formata a segunda hora
 
-print(time_interval)
+time_interval1 = time_2 - time_1 #calcula período 1
+time_interval2 = time_4 - time_3 #calcula período 2
+
+horas_trab = time_interval1 + time_interval2 #soma entre os 2 períodos
+
+
+
+
+print("Período 1 trabalhado: ",time_interval1)
+print("Período 2 trabalhado: ",time_interval2)
+print("Horas Trabalhandas  : ", horas_trab)
+
